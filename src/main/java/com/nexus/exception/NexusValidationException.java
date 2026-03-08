becee57b6bce78166fb9ce644e89cbd37c303f9d
Pedro Sanchez Bitencourt <pedrosanbit@gmail.com>
2026-03-08 17:47:00 -0300

    Cleaned up comments

--- src/main/java/com/nexus/exception/NexusValidationException.java
+++ src/main/java/com/nexus/exception/NexusValidationException.java
@@ -3,7 +3,7 @@ package com.nexus.exception;
 public class NexusValidationException extends RuntimeException {
     public NexusValidationException(String message) {
         super(message);
-        // Dica para o aluno: Incrementar contador global de erros aqui? 
-        // Ou melhor deixar para a Task gerenciar.
+        // Incrementar contador global de erros aqui
+        // Ou melhor deixar para a Task gerenciar?
     }
 }
